3ba63a2ce7b9db059ed73cadcb6faa2ea5a71c57
tcyang <tcyang@gm.pu.edu.tw>
2021-04-19 18:06:10 +0800

    「清除畫布」按鈕

--- app/src/main/java/tw/edu/pu/csim/tcyang/handshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/handshape/MainActivity.kt
@@ -2,10 +2,19 @@ package tw.edu.pu.csim.tcyang.handshape
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                handv.path.reset()
+                handv.invalidate()
+            }
+        })
     }
 }
