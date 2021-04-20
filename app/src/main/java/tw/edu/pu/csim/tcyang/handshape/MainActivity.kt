b845f84d2908a44cf8368ac4dffcfbb50802b72f
tcyang <tcyang@gm.pu.edu.tw>
2021-04-20 08:50:10 +0800

    將塗鴨轉換成Bitmap，供tflite辨識

--- app/src/main/java/tw/edu/pu/csim/tcyang/handshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/handshape/MainActivity.kt
@@ -1,10 +1,15 @@
 package tw.edu.pu.csim.tcyang.handshape
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.tensorflow.lite.support.image.TensorImage
+import tw.edu.pu.csim.tcyang.handshape.ml.Shapes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +30,15 @@ class MainActivity : AppCompatActivity() {
                 when (event.action) {
                     MotionEvent.ACTION_DOWN -> handv.path.moveTo(xPos, yPos)
                     MotionEvent.ACTION_MOVE -> handv.path.lineTo(xPos, yPos)
+
+                    MotionEvent.ACTION_UP -> {
+                        //將handv轉成Bitmap
+                        val b = Bitmap.createBitmap(handv.measuredWidth, handv.measuredHeight,
+                                Bitmap.Config.ARGB_8888)
+                        val c = Canvas(b)
+                        handv.draw(c)
+                        classifyDrawing(b)
+                    }
                 }
                 handv.invalidate()
                 return true
@@ -33,6 +47,20 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    fun classifyDrawing(bitmap : Bitmap) {
+        val model = Shapes.newInstance(this)
 
+        // Creates inputs for reference.
+        val image = TensorImage.fromBitmap(bitmap)
 
+        // Runs model inference and gets result.
+        val outputs = model.process(image)
+        val probability = outputs.probabilityAsCategoryList
+
+        // Releases model resources if no longer used.
+        model.close()
+        Toast.makeText(this, probability.toString(), Toast.LENGTH_SHORT).show()
     }
+
+
+}
